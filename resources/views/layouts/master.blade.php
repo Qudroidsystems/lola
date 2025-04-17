eecdd1fbc5d62a7e95f9b9967005e17f451e16d8
Eliab <eliabsiji@gmail.com>
2025-04-17 19:18:28 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-        <link rel="shortcut icon" href="{{ asset('html/assets/assets/media/logos/favicon.ico')}}"/>
+        <!-- <link rel="shortcut icon" href="{{ asset('html/assets/assets/media/logos/favicon.ico')}}"/> -->
+        <link rel="shortcut icon" href="{{ asset('ruby/ruby/assets/img/favicon.ico')}}" type="image/x-icon"/>
 
         <!--begin::Fonts(mandatory for all pages)-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700"/>
