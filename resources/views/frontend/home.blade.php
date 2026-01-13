497f1bf35782c617e998ed7bb5368d6437801ff2
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-01-14 00:50:38 +0100

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -33,7 +33,7 @@
                                 <div class="single-carousel-wrap home_6_slide_1">
                                     <div class="banner-caption text-center text-lg-start">
                                         <p><h2>LorlahTate</h2></p>
-                                        <h3>Necklace <br> for Princess</h3>
+                                        <h3>Wearable Crafts<br> for Princess</h3>
                                         <p>Elevate your elegance with our exquisite LorlahTate necklace, crafted with precision to enhance your royal charm.</p>
                                         <a href="#" class="btn-long-arrow">Learn More</a>
                                     </div>
@@ -44,7 +44,7 @@
                                 <div class="single-carousel-wrap home_6_slide_2">
                                     <div class="banner-caption text-center text-lg-start">
                                         <p><h2>LorlahTate</h2></p>
-                                        <h3>Necklace <br> for Princess</h3>
+                                        <h3>Wearable Crafts <br> for Princess</h3>
                                         <p>Discover timeless beauty with the LorlahTate necklace, designed to make every moment sparkle with sophistication.</p>
                                         <a href="#" class="btn-long-arrow">Learn More</a>
                                     </div>
