ca2c92d52cc0ddd49282dd9e6e6f621f73e0fdc9
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-01-13 17:05:40 +0100

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -108,7 +108,7 @@ class CartController extends Controller
         if ($cartItem->user_id !== auth()->id()) {
             return response()->json([
                 'success' => false,
-                'message' => 'Unauthorized action.'.$cartItem->user_id.' '. auth()->id()
+                'message' => 'Unauthorized action.'.$cartItem->user_id
             ], 403);
         }
 
